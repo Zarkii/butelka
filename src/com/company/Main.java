f7744d51edc2fcdd9645ecdccda0ee290de50975
Zarkii <35203783+Zarkii@users.noreply.github.com>
2018-07-22 21:35:00 +0200

    dodano pojemnosc,poprawiono funkcje wlej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 
 public class Main {
     double ileLitrow;
+    int pojemnosc;
     Main(){
 
     }
@@ -12,8 +13,16 @@ public class Main {
     double GetIleLitrow(){
         return ileLitrow;
     }
-    void wlej(double ilosc){
+    Main(int pojemnosc){
+        this.pojemnosc = pojemnosc;
+    }
+    boolean wlej(double ilosc){
+        if (ilosc+ileLitrow > pojemnosc){
+            System.out.println("za mała pojemnosc!");
+            return false;
+        }else{
         this.ileLitrow += ilosc;
+        return true;}
     }
     boolean wylej(double ilosc){
         if (ilosc > ileLitrow){
@@ -42,10 +51,13 @@ public class Main {
         butelka[0].ileLitrow = 5;
         butelka[1].ileLitrow = 8;
         butelka[2].ileLitrow = 9;
+        butelka[0].pojemnosc = 12;
+        butelka[1].pojemnosc = 15;
+        butelka[2].pojemnosc = 18;
 
 
-        butelka[2].przelej(32,butelka[0]);
-
+        //butelka[2].przelej(32,butelka[0]);
+        butelka[0].wlej(9);
         for (int i = 0;i <=2;i++){
             System.out.println(butelka[i].GetIleLitrow());
 
