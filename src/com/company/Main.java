2b3df08b5c4e686483ba776ba77e8591ea142db1
Zarkii <35203783+Zarkii@users.noreply.github.com>
2018-07-22 18:21:50 +0200

    dodano funkcje wlej i wylej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,41 @@
 package com.company;
 
+
 public class Main {
+    double ileLitrow;
+    Main(){
+
+    }
+    Main(double ileLitrow){
+        this.ileLitrow = ileLitrow;
+    }
+    double GetIleLitrow(){
+        return ileLitrow;
+    }
+    void wlej(double ilosc){
+        this.ileLitrow += ilosc;
+    }
+    void wylej(double ilosc){
+        this.ileLitrow -= ilosc;
+    }
 
     public static void main(String[] args) {
-        System.out.println("ahg");
-	// write your code here
-    }
-}
+        Main[] butelka = new Main[3];
+
+        for (int i = 0;i <= 2;i++){
+            butelka[i] = new Main();
+
+        }
+        butelka[0].ileLitrow = 5;
+        butelka[1].ileLitrow = 8;
+        butelka[2].ileLitrow = 9;
+
+        for (int i = 0;i <=2;i++){
+            System.out.println(butelka[i].GetIleLitrow());
+
+
+
+        }
+
+    }}
+
