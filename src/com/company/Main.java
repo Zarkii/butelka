c8c6a59a2ab126b7e41eb653f63603c4b38d164f
Zarkii <35203783+Zarkii@users.noreply.github.com>
2018-07-22 18:28:28 +0200

    dodano opcje przelej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,6 +18,10 @@ public class Main {
     void wylej(double ilosc){
         this.ileLitrow -= ilosc;
     }
+    void przelej(double ilosc,Main gdzie){
+        this.wylej(ilosc);
+        gdzie.wlej(ilosc);
+    }
 
     public static void main(String[] args) {
         Main[] butelka = new Main[3];
@@ -30,12 +34,14 @@ public class Main {
         butelka[1].ileLitrow = 8;
         butelka[2].ileLitrow = 9;
 
+
+        butelka[2].przelej(3,butelka[0]);
+
         for (int i = 0;i <=2;i++){
             System.out.println(butelka[i].GetIleLitrow());
 
 
 
         }
-
     }}
 
